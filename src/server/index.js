8d18f3006eea6662a007e402e33bd759af3a3902
pablomartan <pablomartan@kolabnow.com>
2022-12-31 12:14:09 +0100

    feat: added a basic html skeleton, created an empty index.js file on client/ and updated server/index.js settings to have webpack run without errors

--- src/server/index.js
+++ src/server/index.js
@@ -3,6 +3,12 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.static('dist'));
+
+app.get('/', function(req, res) {
+    res.sendFile('dist/index.html');
+});
+
 app.listen(8080, () => {
     console.log("Server running on port 8080");
 });
