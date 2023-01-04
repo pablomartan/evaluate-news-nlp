948b7fa99ca5ae7aedca7240403d81437b2871f3
pablomartan <pablomartan@kolabnow.com>
2023-01-04 11:04:30 +0100

    feat: updated index.js

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,12 @@
+import { handleSubmit } from './js/client'
+
+import './styles/reset.scss'
 import './styles/base.scss'
 import './styles/header.scss'
 import './styles/footer.scss'
 import './styles/form.scss'
 import './styles/main.scss'
+
+export {
+    handleSubmit
+}
